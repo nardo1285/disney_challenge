22c9e29913fee2c1bc5316dbe0b2070354e65485
nardo1285 <nardo1285@hotmail.com>
2021-12-09 10:11:48 -0300

    Controller Genero

--- src/main/java/com/alkemy/disney_ch/main/controller/GeneroController.java
+++ src/main/java/com/alkemy/disney_ch/main/controller/GeneroController.java
@@ -2,6 +2,8 @@
 package com.alkemy.disney_ch.main.controller;
 
 import com.alkemy.disney_ch.main.dto.GeneroDTO;
+import com.alkemy.disney_ch.main.services.GeneroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,11 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/genero")
 public class GeneroController {
     
+    @Autowired
+    private GeneroService genService;
+    
     @PostMapping("")
     public ResponseEntity<GeneroDTO> save(@RequestBody GeneroDTO genero){
         
+        GeneroDTO generoSaved = genService.save(genero);
         
-        return ResponseEntity.status(HttpStatus.CREATED).body(generoCreado);
+        return ResponseEntity.status(HttpStatus.CREATED).body(generoSaved);
     }
     
     
--- src/main/java/com/alkemy/disney_ch/main/services/GeneroService.java
+++ src/main/java/com/alkemy/disney_ch/main/services/GeneroService.java
@@ -1,10 +1,16 @@
 
 package com.alkemy.disney_ch.main.services;
 
+import com.alkemy.disney_ch.main.dto.GeneroDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GeneroService {
     
-    public GeneroDTO save()
+    public GeneroDTO save(GeneroDTO dto){
+        
+        System.out.println("Guardar Género");
+        
+        return dto;
+    }
 }
